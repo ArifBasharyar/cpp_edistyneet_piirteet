70033dad5102837c947e8d9683da3e5e9f7ff362
Arif Basharyar <saye.arif@gmail.com>
2023-03-15 18:58:08 +0200

    Update and improve file searcher function using lambda.

--- Harjoitustyo/fncs.cpp
+++ Harjoitustyo/fncs.cpp
@@ -1,6 +1,10 @@
 #include "fncs.h"
 
-bool search_within_string(const string *const str1, const string *const str2, size_t *const pos) {
+bool search_within_string(
+    const string *const str1,
+    const string *const str2,
+    size_t *const pos
+) {
     // Tarkistaa koot
     if (str1->length() < str2->length())
         return false;
@@ -8,7 +12,7 @@ bool search_within_string(const string *const str1, const string *const str2, si
     // Etsii str2 str1:stä
     size_t temp = str1->find(*str2);
 
-    // Jos kaikki mennyt hyvin muuttaa line_positionin arvon
+    // Jos kaikki mennyt hyvin muuttaa pos arvon ja palauttaa arvon true
     if (temp != string::npos) {
         *pos = temp;
         return true;
@@ -18,15 +22,17 @@ bool search_within_string(const string *const str1, const string *const str2, si
     return false;
 }
 
-bool search_within_string(const string *const str1, const string *const str2) {
+bool search_within_string(
+    const string *const str1,
+    const string *const str2
+) {
     // Tarkista koot
-    if (str1->length() < str2->length())
-        return false;
+    if (str1->length() < str2->length()) return false;
 
     // Etsi str2 str1:stä
     size_t temp = str1->find(*str2);
 
-    // Jos kaikki mennyt hyvin muuttaa line_positionin arvon
+    // Jos kaikki mennyt hyvin palauttaa arvon true
     if (temp != string::npos)
         return true;
 
@@ -34,7 +40,10 @@ bool search_within_string(const string *const str1, const string *const str2) {
     return false;
 }
 
-void option_switch(string *const str, print_opt *const opt) {
+void option_switch(
+    string *const str,
+    print_opt *const opt
+) {
     // Käytetään stringiä ilman -o:ta
     str->erase(0, 2);
 
@@ -51,7 +60,11 @@ void option_switch(string *const str, print_opt *const opt) {
     if (search_within_string(str, &i)) opt->i = true;
 }
 
-void read_file(const string *const str1, const string *const str2, const print_opt *const opt) {
+void read_file(
+    const string *const str1,
+    const string *const str2,
+    const print_opt *const opt
+) {
     // Muuttujat
     std::ifstream infile(*str1);
     size_t line{}, occ{}, nocc{};
@@ -62,8 +75,17 @@ void read_file(const string *const str1, const string *const str2, const print_o
     if (!infile) throw - 1;
 
     // Jos halutaan, muuttaa haku stringin kirjaimet alemmaksi
-    if (opt->i)
-        std::transform(low_str.begin(), low_str.end(), low_str.begin(), ::tolower);
+    if (opt->i) std::transform(low_str.begin(), low_str.end(),
+                               low_str.begin(), ::tolower);
+
+    const auto print_line = [&]() {
+        // Lisää haluttua laskuria yhdellä
+        opt->r ? nocc += 1 : occ += 1;
+        // Jos halutaan tulostaa rivinumero
+        if (opt->l) std::cout << line << ":";
+        // Tulostetaan haluttu rivi
+        std::cout << temp1 << "\n";
+    };
 
     // Etsii haku strigiä
     while (getline(infile, temp1)) {
@@ -73,41 +95,33 @@ void read_file(const string *const str1, const string *const str2, const print_o
         if (opt->i) {
             // Käytetään temp2 jotta voidaan tulostaa alkuperäinen rivi temp1
             temp2 = temp1;
-            std::transform(temp2.begin(), temp2.end(), temp2.begin(), ::tolower);
-
-            // Jos halutaan tulostaa rivi, kun alennettua stringiä ei löydy
-            if (!search_within_string(&temp2, &low_str) && opt->r)
-                print_opt_lines(&nocc, &opt->l, &line, &temp1);
+            std::transform(temp2.begin(), temp2.end(),
+                           temp2.begin(), ::tolower);
 
             // Jos halutaan tulostaa rivi, kun alennettu haku string löytyy
-            else if (search_within_string(&temp2, &low_str) && !opt->r)
-                print_opt_lines(&occ, &opt->l, &line, &temp1);
+            if (search_within_string(&temp2, &low_str) && !opt->r)
+                print_line();
+
+            // Jos halutaan tulostaa rivi, kun alennettua stringiä ei löydy
+            else if (!search_within_string(&temp2, &low_str) && opt->r)
+                print_line();
 
         } else {
-            // Jos halutaan tulostaa rivi, kun stringiä ei löydy
-            if (!search_within_string(&temp1, str2) && opt->r)
-                print_opt_lines(&nocc, &opt->l, &line, &temp1);
-
             // Jos halutaan tulostaa rivi, kun haku string löytyy
-            else if (search_within_string(&temp1, str2) && !opt->r)
-                print_opt_lines(&occ, &opt->l, &line, &temp1);
+            if (search_within_string(&temp1, str2) && !opt->r)
+                print_line();
+
+            // Jos halutaan tulostaa rivi, kun stringiä ei löydy
+            else if (!search_within_string(&temp1, str2) && opt->r)
+                print_line();
         }
     }
+
     // Jos halutaan tulostaa rivien määrä
-    if (opt->o && !opt->r)
-        std::cout << "\nOccurrences of lines containing \"" << *str2 << "\": " << occ << "\n";
-    if (opt->o && opt->r)
-        std::cout << "\nOccurrences of lines NOT containing \"" << *str2 << "\": " << nocc << "\n";
+    if (opt->o && !opt->r) std::cout << "\nOccurrences of lines containing \""
+        << *str2 << "\": " << occ << "\n";
+    if (opt->o && opt->r) std::cout << "\nOccurrences of lines NOT containing \""
+        << *str2 << "\": " << nocc << "\n";
 
     infile.close();
 }
-
-void print_opt_lines(size_t *const ctr, const bool *const optl, const size_t *const num, const string *const str) {
-    // Lisää haluttua laskuria yhdellä
-    *ctr += 1;
-    // Jos halutaan tulostaa rivinumero
-    if (*optl) std::cout << *num << ":";
-    // Tulostetaan haluttu rivi
-    std::cout << *str << "\n";
-}
-
--- Harjoitustyo/fncs.h
+++ Harjoitustyo/fncs.h
@@ -13,11 +13,24 @@ struct print_opt {
     bool i;
 };
 
-bool search_within_string(const string *const str1, const string *const str2, size_t *const pos);
-bool search_within_string(const string *const str1, const string *const str2);
-void option_switch(string *const str, print_opt *const opt);
-void read_file(const string *const str1, const string *const str2, const print_opt *const opt);
-void print_opt_lines(size_t *const ctr, const bool *const optl, const size_t *const num, const string *const str);
+bool search_within_string(
+    const string *const str1,
+    const string *const str2,
+    size_t *const pos
+);
+bool search_within_string(
+    const string *const str1,
+    const string *const str2
+);
+void option_switch(
+    string *const str,
+    print_opt *const opt
+);
+void read_file(
+    const string *const str1,
+    const string *const str2,
+    const print_opt *const opt
+);
 
 // -olori following man_grep_plain_ASCII.txt
 
